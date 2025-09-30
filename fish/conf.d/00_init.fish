241fb3cc0be3e97673cafea06882e7bb98acd596
lucas boulle <boulle@lucass-MacBook-Pro.local>
2025-09-30 08:02:40 -0300

    update fish init for macos initialization

--- fish/conf.d/00_init.fish
+++ fish/conf.d/00_init.fish
@@ -1,22 +1,28 @@
 set fish_greeting
 
-# starttup starship prompt
+# Startup starship prompt
 starship init fish | source
 
-# Android SDK setup
-set -x ANDROID_HOME $HOME/Android/Sdk
-set -x ANDROID_SDK_ROOT $HOME/Android/Sdk
+# Detect OS
+set -l OS (uname)
 
-# Add tools to PATH
-set -x PATH $ANDROID_HOME/emulator $PATH
-set -x PATH $ANDROID_HOME/tools $PATH
-set -x PATH $ANDROID_HOME/tools/bin $PATH
-set -x PATH $ANDROID_HOME/platform-tools $PATH
+if test "$OS" != "Darwin"
+    # Linux-only configuration
 
-status --is-interactive; and begin
-    set -l keys github-void-avell id_rsa  # list your key filenames in ~/.ssh
-    eval (keychain --eval --quiet --agents ssh $keys)
+    # Android SDK setup
+    set -x ANDROID_HOME $HOME/Android/Sdk
+    set -x ANDROID_SDK_ROOT $HOME/Android/Sdk
+
+    # Add tools to PATH
+    set -x PATH $ANDROID_HOME/emulator $PATH
+    set -x PATH $ANDROID_HOME/tools $PATH
+    set -x PATH $ANDROID_HOME/tools/bin $PATH
+    set -x PATH $ANDROID_HOME/platform-tools $PATH
+
+    # Only run keychain in interactive Linux shells
+    status --is-interactive; and begin
+        set -l keys github-void-avell id_rsa
+        eval (keychain --eval --quiet --agents ssh $keys)
+    end
 end
-
-
 
